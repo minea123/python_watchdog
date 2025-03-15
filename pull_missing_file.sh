3cdfd1bac14ac93e8fd379a8f64299ed422b10e3
Codingxxxxx <hc2.minea@gmail.com>
2025-03-15 17:08:38 +0700

    fix pull missing script

--- pull_missing_file.sh
+++ pull_missing_file.sh
@@ -6,6 +6,7 @@ echo "start initial syncronize..."
 
 IFS=',' read -r -a ip_array <<< "$(echo $SERVER_TARGET)"
 for ip in "${ip_array[@]}"; do
+    ip=$(echo "$ip" | sed 's/http:\/\///' | sed 's/:.*//')
     echo "Start initial sync from server $ip"
     SSH_SERVER=root@$ip
     rsync -avz --progress --ignore-existing --exclude 'mpdf' $SSH_SERVER:/App/aii_school_prod/storage/app /App/aii_school_prod/storage
