db6b98efb1ce7515b24c08b4d719722ab889045b
Codingxxxxx <hc2.minea@gmail.com>
2025-03-15 17:05:43 +0700

    upload using api instead of sscp

--- kill.sh
+++ kill.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 ps aux | grep -v grep | grep watch_dog | awk '{print $2}' | xargs -r sudo kill -9
-ps aux | grep -v grep | grep "tail -f -n 0" | awk '{print $2}' | xargs -r sudo kill -9
--- watch_dog.py
+++ watch_dog.py
@@ -4,21 +4,43 @@ from watchdog.events import FileSystemEvent, PatternMatchingEventHandler
 from watchdog.observers import Observer
 from dotenv import load_dotenv
 import os
+import requests
+from concurrent.futures import ThreadPoolExecutor
+from threading import current_thread
 
-load_dotenv('.env')
-
+load_dotenv('.env', override=True) 
 log = logger()
+servers = os.getenv('SERVER_TARGET').split(',')
+
+log.info(f'Available servers'  + str(servers))
+
+def upload(filePath: str):
+    thread_name = current_thread().name
+    log.info(f'Uploading file using thread {thread_name} {filePath}')
+    
+    form_data = {
+        'destination': filePath
+    }
+
+    with open(filePath, 'rb') as f:
+        files = {'file': f}
+
+        for server in servers:
+            try:
+                response = requests.post(f'{server}/api/upload', data=form_data, files=files)
+                response.raise_for_status()
+            except Exception as e:
+                log.error(f'Failed to upload to server {server}: {e} {filePath}')
 
 class MyEventHandler(PatternMatchingEventHandler):
     def __init__(self, *, patterns = None, ignore_patterns = None, ignore_directories = False, case_sensitive = False):
         super().__init__(ignore_patterns=["*/.*", "*/*mpdf*/*"], ignore_directories=True)
-        self.file_handle = open(os.getenv('APPEND_LOG'), 'a')
+        self.executor = ThreadPoolExecutor(max_workers=100)
 
     def on_created(self, event: FileSystemEvent) -> None:
         try:
             log.info(f'EVENT:CREATE {event.src_path}')
-            self.file_handle.write(f'CREATE:{event.src_path}\n')
-            self.file_handle.flush()
+            self.executor.submit(upload, event.src_path)
         except Exception as ex:
             log.error(f'EXCEPTION: {ex}')
 
@@ -26,6 +48,7 @@ event_handler = MyEventHandler()
 observer = Observer()
 observer.schedule(event_handler, "/App/aii_school_prod/storage/app", recursive=True)
 observer.start()
+
 try:
     while True:
         time.sleep(1)
