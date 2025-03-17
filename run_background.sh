38ae96a7724fcb65c6d406a86333155a740aa9b2
minea123 <chanminea.houn@mjqeducation.edu.kh>
2025-03-17 10:40:07 +0700

    fix script run background

--- run_background.sh
+++ run_background.sh
@@ -4,4 +4,4 @@ cd $PWD
 sudo ./kill.sh
 ./pull_missing_file.sh
 
-source .venv/bin/activate && nohup python ./watch_dog.py >> watchdog.log 2 >> watchdog.error.log &
+source .venv/bin/activate && nohup python ./watch_dog.py >> watchdog.log 2>&1 &
